68c7ed4f006a484cd97c265f429b02e23d0a4418
Celeste6666 <aafree2014@gmail.com>
2023-01-04 17:17:02 +0800

    增加路由懶加載

--- src/routes.js
+++ src/routes.js
@@ -1,54 +1,76 @@
-import App from './App';
-import AppClear from './components/AppClear';
-import Intro from './pages/Intro';
-import Role from './pages/Role';
-import Backlog from './pages/Backlog';
-import BacklogGame from './components/BacklogGame';
-import Planning from './pages/Planning';
-import PlanningChat from './components/PlanningChat';
-import PlanningPoint from './components/PlanningPoint';
-import Sprint from './pages/Sprint';
-import SprintExplanation from './components/SprintExplanation';
-import SprintGame from './components/SprintGame';
-import Retrospective from './pages/Retrospective';
-import RetrospectiveChat from './components/RetrospectiveChat';
-import Material from './pages/Material';
-import Award from './pages/Award';
+import { lazy, Suspense } from "react";
+import App from "./App";
+import Loader from "./components/general/Loader";
+const AppClear = lazy(() => import("./components/AppClear"));
+const Intro = lazy(() => import("./pages/Intro"));
+const Role = lazy(() => import("./pages/Role"));
+const Backlog = lazy(() => import("./pages/Backlog"));
+const BacklogGame = lazy(() => import("./components/BacklogGame"));
+const Planning = lazy(() => import("./pages/Planning"));
+const PlanningChat = lazy(() => import("./components/PlanningChat"));
+const PlanningPoint = lazy(() => import("./components/PlanningPoint"));
+const Sprint = lazy(() => import("./pages/Sprint"));
+const SprintExplanation = lazy(() => import("./components/SprintExplanation"));
+const SprintGame = lazy(() => import("./components/SprintGame"));
+const Retrospective = lazy(() => import("./pages/Retrospective"));
+const RetrospectiveChat = lazy(() => import("./components/RetrospectiveChat"));
+const Material = lazy(() => import("./pages/Material"));
+const Award = lazy(() => import("./pages/Award"));
 const routes = [
   {
-    path: '/',
+    path: "/",
     element: <App />,
     children: [
       {
-        path: 'clear',
-        element: <AppClear />,
+        path: "clear",
+        element: (
+          <Suspense fallback={<Loader />}>
+            <AppClear />
+          </Suspense>
+        ),
       },
     ],
   },
   {
-    path: '/intro',
-    element: <Intro />,
+    path: "/intro",
+    element: (
+      <Suspense fallback={<Loader />}>
+        <Intro />
+      </Suspense>
+    ),
     loader: () => {
       return 1;
     },
   },
   {
-    path: '/role/:roleId',
-    element: <Role />,
+    path: "/role/:roleId",
+    element: (
+      <Suspense fallback={<Loader />}>
+        <Role />
+      </Suspense>
+    ),
     loader: () => {
       return 2;
     },
   },
   {
-    path: '/backlog',
-    element: <Backlog />,
+    path: "/backlog",
+    element: (
+      <Suspense>
+        <Backlog />
+      </Suspense>
+    ),
     loader: () => {
       return 3;
     },
     children: [
       {
-        path: 'game',
-        element: <BacklogGame />,
+        path: "game",
+        element: (
+          <Suspense>
+            <BacklogGame />
+          </Suspense>
+        ),
         loader: () => {
           return 3;
         },
@@ -56,22 +78,34 @@ const routes = [
     ],
   },
   {
-    path: '/planning',
-    element: <Planning />,
+    path: "/planning",
+    element: (
+      <Suspense>
+        <Planning />
+      </Suspense>
+    ),
     loader: () => {
       return 4;
     },
     children: [
       {
-        path: 'chat',
-        element: <PlanningChat />,
+        path: "chat",
+        element: (
+          <Suspense>
+            <PlanningChat />
+          </Suspense>
+        ),
         loader: () => {
           return 4;
         },
       },
       {
-        path: 'point',
-        element: <PlanningPoint />,
+        path: "point",
+        element: (
+          <Suspense>
+            <PlanningPoint />
+          </Suspense>
+        ),
         loader: () => {
           return 5;
         },
@@ -79,22 +113,34 @@ const routes = [
     ],
   },
   {
-    path: '/sprint',
-    element: <Sprint />,
+    path: "/sprint",
+    element: (
+      <Suspense>
+        <Sprint />
+      </Suspense>
+    ),
     loader: () => {
       return 6;
     },
     children: [
       {
-        path: 'explanation',
-        element: <SprintExplanation />,
+        path: "explanation",
+        element: (
+          <Suspense>
+            <SprintExplanation />
+          </Suspense>
+        ),
         loader: () => {
           return 6;
         },
       },
       {
-        path: 'game',
-        element: <SprintGame />,
+        path: "game",
+        element: (
+          <Suspense>
+            <SprintGame />
+          </Suspense>
+        ),
         loader: () => {
           return 6;
         },
@@ -102,15 +148,23 @@ const routes = [
     ],
   },
   {
-    path: '/retrospective',
-    element: <Retrospective />,
+    path: "/retrospective",
+    element: (
+      <Suspense>
+        <Retrospective />
+      </Suspense>
+    ),
     loader: () => {
       return 7;
     },
     children: [
       {
-        path: 'chat',
-        element: <RetrospectiveChat />,
+        path: "chat",
+        element: (
+          <Suspense>
+            <RetrospectiveChat />
+          </Suspense>
+        ),
         loader: () => {
           return 7;
         },
@@ -118,15 +172,23 @@ const routes = [
     ],
   },
   {
-    path: '/material',
-    element: <Material />,
+    path: "/material",
+    element: (
+      <Suspense>
+        <Material />
+      </Suspense>
+    ),
     loader: () => {
       return 8;
     },
   },
   {
-    path: '/award',
-    element: <Award />,
+    path: "/award",
+    element: (
+      <Suspense>
+        <Award />
+      </Suspense>
+    ),
   },
 ];
 
